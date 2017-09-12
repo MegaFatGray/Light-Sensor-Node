a715ff678503fb5c67df604d5a649e31ab0ec2a4
MegaFatGray <31743397+MegaFatGray@users.noreply.github.com>
2017-09-12 19:23:01 +0100

    Polling appears to work

--- Src/mg/mg_adc.c
+++ Src/mg/mg_adc.c
@@ -50,36 +50,75 @@ extern UART_HandleTypeDef huart1;
 // functions
 void mg_adc_GetReading(void)
 {
-	HAL_ADC_Start(&hadc);
-	//HAL_UART_Transmit(&huart1, (uint8_t)AdcStatus, sizeof(AdcStatus), 500);
+	AdcStatus = HAL_ADC_Start(&hadc);
+	
+	char AdcStatusString[50];
+	switch(AdcStatus)
+	{
+		case HAL_OK:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_Start HAL_OK");
+			break;
+		}
+		case HAL_ERROR:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_Start HAL_ERROR");
+			break;
+		}
+		case HAL_BUSY:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_Start HAL_BUSY");
+			break;
+		}
+		case HAL_TIMEOUT:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_Start HAL_TIMEOUT");
+			break;
+		}
+		default:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_Start defaulted");
+			break;
+		}
+	}
+	HAL_UART_Transmit(&huart1, (uint8_t*)AdcStatusString, strlen(AdcStatusString), 500);
 	
 	AdcStatus = HAL_ADC_PollForConversion(&hadc, PollForConversionTimeout);
 	
 	switch(AdcStatus)
 	{
-		char AdcStatusString[20];
-			
 		case HAL_OK:
 		{
-			strcpy(AdcStatusString, "\n\rHAL_OK");
+			strcpy(AdcStatusString, "\n\rHAL_ADC_PollForConversion HAL_OK");
+			break;
 		}
 		case HAL_ERROR:
 		{
-			strcpy(AdcStatusString, "\n\rHAL_ERROR");
+			strcpy(AdcStatusString, "\n\rHAL_ADC_PollForConversion HAL_ERROR");
+			break;
 		}
 		case HAL_BUSY:
 		{
-			strcpy(AdcStatusString, "\n\rHAL_BUSY");
+			strcpy(AdcStatusString, "\n\rHAL_ADC_PollForConversion HAL_BUSY");
+			break;
 		}
 		case HAL_TIMEOUT:
 		{
-			strcpy(AdcStatusString, "\n\rHAL_TIMEOUT");
+			strcpy(AdcStatusString, "\n\rHAL_ADC_PollForConversion HAL_TIMEOUT");
+			break;
 		}
-		
-		HAL_UART_Transmit(&huart1, (uint8_t*)AdcStatusString, strlen(AdcStatusString), 500);
-		
+		default:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_PollForConversion defaulted");
+			break;
+		}
 	}
+	HAL_UART_Transmit(&huart1, (uint8_t*)AdcStatusString, strlen(AdcStatusString), 500);
 	
+	uint32_t AdcReading = HAL_ADC_GetValue(&hadc);
+	char AdcReadingString[50];
+	sprintf(AdcReadingString, "\n\rADC Reading = %d", AdcReading);
+	HAL_UART_Transmit(&huart1, (uint8_t*)AdcReadingString, strlen(AdcReadingString), 500);
 }
 
 // close the Doxygen group
