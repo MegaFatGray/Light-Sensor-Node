132985df363deb484d7d3306bce9baeb84b5a3f9
MegaFatGray <31743397+MegaFatGray@users.noreply.github.com>
2017-09-12 18:58:49 +0100

    Interrupts attempt - not working

--- Src/mg/mg_adc.c
+++ Src/mg/mg_adc.c
@@ -48,10 +48,72 @@ extern UART_HandleTypeDef huart1;
   
 /*****************************************************************************/
 // functions
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+	char HereString[50] 		= "\n\rcallback - conversion complete";
+	HAL_UART_Transmit(&huart1, (uint8_t*)HereString, strlen(HereString), 500);
+}
+
+void HAL_ADC_ErrorCallback(ADC_HandleTypeDef *hadc)
+{
+	char HereString[50] 		= "\n\rcallback - error";
+	HAL_UART_Transmit(&huart1, (uint8_t*)HereString, strlen(HereString), 500);
+}
+	
+void HAL_ADC_LevelOutOfWindowCallback(ADC_HandleTypeDef* hadc)
+{
+	char HereString[50] 		= "\n\rcallback - level out of window";
+	HAL_UART_Transmit(&huart1, (uint8_t*)HereString, strlen(HereString), 500);
+}
+
+void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc)
+{
+	char HereString[50] 		= "\n\rcallback - half complete";
+	HAL_UART_Transmit(&huart1, (uint8_t*)HereString, strlen(HereString), 500);
+}
+
+
 void mg_adc_GetReading(void)
 {
-	HAL_ADC_Start(&hadc);
+	HAL_Delay(1);
+	
+	AdcStatus = HAL_ADC_Start(&hadc);
 	//HAL_UART_Transmit(&huart1, (uint8_t)AdcStatus, sizeof(AdcStatus), 500);
+	char AdcStatusString[20];
+	switch(AdcStatus)
+	{
+		case HAL_OK:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_Start HAL_OK");
+			break;
+		}
+		case HAL_ERROR:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_Start HAL_ERROR");
+			break;
+		}
+		case HAL_BUSY:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_Start HAL_BUSY");
+			break;
+		}
+		case HAL_TIMEOUT:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_Start HAL_TIMEOUT");
+			break;
+		}
+		default:
+		{
+			strcpy(AdcStatusString, "\n\rHAL_ADC_Start defaulted");
+			break;
+		}
+	}
+	HAL_UART_Transmit(&huart1, (uint8_t*)AdcStatusString, strlen(AdcStatusString), 500);
+	
+	/*
+	while(1)
+	{
 	
 	AdcStatus = HAL_ADC_PollForConversion(&hadc, PollForConversionTimeout);
 	
@@ -61,24 +123,27 @@ void mg_adc_GetReading(void)
 			
 		case HAL_OK:
 		{
-			strcpy(AdcStatusString, "\n\rHAL_OK");
+			strcpy(AdcStatusString, "\n\rHAL_ADC_PollForConversion HAL_OK");
 		}
 		case HAL_ERROR:
 		{
-			strcpy(AdcStatusString, "\n\rHAL_ERROR");
+			strcpy(AdcStatusString, "\n\rHAL_ADC_PollForConversion HAL_ERROR");
 		}
 		case HAL_BUSY:
 		{
-			strcpy(AdcStatusString, "\n\rHAL_BUSY");
+			strcpy(AdcStatusString, "\n\rHAL_ADC_PollForConversion HAL_BUSY");
 		}
 		case HAL_TIMEOUT:
 		{
-			strcpy(AdcStatusString, "\n\rHAL_TIMEOUT");
+			strcpy(AdcStatusString, "\n\rHAL_ADC_PollForConversion HAL_TIMEOUT");
 		}
 		
 		HAL_UART_Transmit(&huart1, (uint8_t*)AdcStatusString, strlen(AdcStatusString), 500);
 		
 	}
+	
+	}
+	*/
 	
 }
 
